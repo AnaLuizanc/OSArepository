3aaf2580a589cce646a6243c952159c6d5ccbda7
AnaLuizanc <analuizanc13@gmail.com>
2024-11-30 20:58:50 -0300

    testes prontos no main

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -11,112 +11,61 @@ const int MAX_NOME = 60;
 
 using namespace std;
 
-void testArquivo() {
-    Arquivo arquivoCSV;
-    arquivoCSV.nomeArquivo = "teste_registros.csv";
-
-    // Cria um arquivo CSV de teste
-    ofstream outFileCSV(arquivoCSV.nomeArquivo);
-    outFileCSV << "Joao,25\nMaria,30\nPedro,22\n";
-    outFileCSV.close();
-
-    // Lê registros do arquivo CSV
-    vector<Registro> registrosCSV = arquivoCSV.lerRegistrosCSV();
-    assert(registrosCSV.size() == 3);
-    assert(registrosCSV[0].nome == "Joao");
-    assert(registrosCSV[0].idade == 25);
-    assert(registrosCSV[1].nome == "Maria");
-    assert(registrosCSV[1].idade == 30);
-    assert(registrosCSV[2].nome == "Pedro");
-    assert(registrosCSV[2].idade == 22);
-
-    Arquivo arquivoBinario;
-    arquivoBinario.nomeArquivo = "teste_registros.dat";
-
-    // Adiciona registros fixos ao arquivo binário
-    for (const auto& reg : registrosCSV) {
-        arquivoBinario.adicionarRegistroFixo(reg);
-    }
-
-    // Lê registros fixos do arquivo binário
-    vector<Registro> registrosBinario = arquivoBinario.lerRegistrosFixo();
-    assert(registrosBinario.size() == 3);
-    assert(registrosBinario[0].nome == "Joao");
-    assert(registrosBinario[0].idade == 25);
-    assert(registrosBinario[1].nome == "Maria");
-    assert(registrosBinario[1].idade == 30);
-    assert(registrosBinario[2].nome == "Pedro");
-    assert(registrosBinario[2].idade == 22);
-
-    cout << "Arquivo::Todos os testes passaram!" << endl;
-}
-
-void testBuffer() {
-    // Cria um objeto Registro e define os valores dos atributos
-    Registro reg1;
-    reg1.nome = "Joao";
-    reg1.idade = 25;
-
-    // Abre um arquivo para escrita
-    ofstream outFile("teste_registro.dat", ios::binary);
-    Buffer buffer;
-    buffer.escreverRegistroFixo(reg1, outFile);
-    outFile.close();
-
-    // Abre o arquivo para leitura
-    ifstream inFile("teste_registro.dat", ios::binary);
-    Registro reg2 = buffer.lerRegistroFixo(inFile);
-    inFile.close();
-
-    // Verifica se os valores dos atributos foram preservados
-    assert(reg2.nome == "Joao");
-    assert(reg2.idade == 25);
-
-    cout << "Buffer::Todos os testes passaram!" << endl;
-}
-
-void testRegistro() {
-    // Cria um objeto Registro e define os valores dos atributos
-    Registro reg1;
-    reg1.nome = "Joao";
-    reg1.idade = 25;
-
-    // Serializa o objeto
-    string buffer = reg1.packFixed();
-
-    // Cria um novo objeto Registro e desserializa o buffer
-    Registro reg2;
-    reg2.unpackFixed(buffer);
-
-    // Verifica se os valores dos atributos foram preservados
-    assert(reg2.nome == "Joao");
-    assert(reg2.idade == 25);
-
-    // Teste com nome maior que MAX_NOME
-    Registro reg3;
-    reg3.nome = "NomeMuitoGrandeParaTestarSeFuncionaCorretamente";
-    reg3.idade = 30;
-
-    // Serializa o objeto
-    buffer = reg3.packFixed();
-
-    // Cria um novo objeto Registro e desserializa o buffer
-    Registro reg4;
-    reg4.unpackFixed(buffer);
-
-    // Verifica se os valores dos atributos foram preservados
-    assert(reg4.nome == std::string("NomeMuitoGrandeParaTestarSeFuncionaCorretamente").substr(0, MAX_NOME));
-    assert(reg4.idade == 30);
-
-    cout << "Registro::Todos os testes passaram!" << endl;
-}
-
-
 int main()
 {
-    // Registro r1;
-    testRegistro();
-    testBuffer();
-    testArquivo();
+    Arquivo csv1, csv2, csv3, csv4, csv5;
+    csv1.nomeArquivo = "Nomes_Idades_1.csv";
+    csv2.nomeArquivo = "Nomes_Idades_2.csv";
+    csv3.nomeArquivo = "Nomes_Idades_3.csv";
+    csv4.nomeArquivo = "Nomes_Idades_4.csv";
+    csv5.nomeArquivo = "Nomes_Idades_5.csv";
+    vector<Registro> registrosCSV1 = csv1.lerRegistrosCSV();
+    vector<Registro> registrosCSV2 = csv2.lerRegistrosCSV();
+    vector<Registro> registrosCSV3 = csv3.lerRegistrosCSV();
+    vector<Registro> registrosCSV4 = csv4.lerRegistrosCSV();
+    vector<Registro> registrosCSV5 = csv5.lerRegistrosCSV();
+
+    Arquivo bin1, bin2, bin3, bin4, bin5;
+    
+    bin1.nomeArquivo = "Nomes_Idades_1.bin";
+    for(const auto& reg : registrosCSV1)
+        bin1.adicionarRegistroFixo(reg);
+
+    bin2.nomeArquivo = "Nomes_Idades_2.bin";
+    for(const auto& reg : registrosCSV2)
+        bin2.adicionarRegistroFixo(reg);
+
+    bin3.nomeArquivo = "Nomes_Idades_3.bin";
+    for(const auto& reg : registrosCSV3)
+        bin3.adicionarRegistroFixo(reg);
+
+    bin4.nomeArquivo = "Nomes_Idades_4.bin";
+    for(const auto& reg : registrosCSV4)
+        bin4.adicionarRegistroFixo(reg);
+
+    bin5.nomeArquivo = "Nomes_Idades_5.bin";
+    for(const auto& reg : registrosCSV5)
+        bin5.adicionarRegistroFixo(reg);
+
+    vector<Registro> registrosBin1 = bin1.lerRegistrosFixo();
+    for (const auto& reg : registrosBin1)
+        cout << reg.nome << " " << reg.idade << endl;
+    
+    vector<Registro> registrosBin2 = bin2.lerRegistrosFixo();
+    for (const auto& reg : registrosBin2)
+        cout << reg.nome << " " << reg.idade << endl;
+    
+    vector<Registro> registrosBin3 = bin3.lerRegistrosFixo();
+    for (const auto& reg : registrosBin3)
+        cout << reg.nome << " " << reg.idade << endl;
+    
+    vector<Registro> registrosBin4 = bin4.lerRegistrosFixo();
+    for (const auto& reg : registrosBin4)
+        cout << reg.nome << " " << reg.idade << endl;
+    
+    vector<Registro> registrosBin5 = bin5.lerRegistrosFixo();
+    for (const auto& reg : registrosBin5)
+        cout << reg.nome << " " << reg.idade << endl;
+
     return 0;
 }
