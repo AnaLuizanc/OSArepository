4ea50c584722871db13bc2f7d8ac2b3c15d36351
AnaLuizanc <analuizanc13@gmail.com>
2024-12-01 21:37:42 -0300

    adiciona classe Livro

---
+++ lab4/Livro.cpp
@@ -0,0 +1,14 @@
+#include "Livro.hpp"
+
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+string Livro::packFixed() const {
+
+}
+
+void Livro::unpackFixed(const string& buffer) {
+
+}
