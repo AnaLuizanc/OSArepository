60a9549cff0c574313d324dde9a57dcde55b7afd
AnaLuizanc <analuizanc13@gmail.com>
2025-03-09 00:30:50 -0300

    refatora a implementação da árvore B, renomeando variáveis e métodos para maior clareza

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,170 +1,158 @@
+// C++ Program to Implement B-Tree
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
+// class for the node present in a B-Tree
 template <typename T>
-class BTreeNode{
-    public:
-        vector<T> chaves;
-        vector<BTreeNode*> filhos;
-        int n; // qtd de chaves
-        bool ehFolha;
+class BTreeNode {
+public:
+    // Vector of keys
+    vector<T> keys;
+    // Vector of child pointers
+    vector<BTreeNode*> children;
+    // Current number of keys
+    int n;
+    // True if leaf node, false otherwise
+    bool leaf;
 
-        BTreeNode(int ordem, bool folha = true) : n(0), ehFolha(folha), chaves(ordem*2), filhos(ordem*2+1, nullptr){}
-
+    BTreeNode(int order, bool isLeaf = true) : n(0), leaf(isLeaf) {
+        keys.resize(2 * order);
+        children.resize(2 * order + 1, nullptr);
+    }
 };
 
 // class for B-Tree
 template <typename T>
-class BTree{
-    private:
-        BTreeNode<T>* raiz; // Ponteiro para o nó da raiz
-        int ordem;
+class BTree {
+private:
+    BTreeNode<T>* root; // Pointer to root node
+    int order; // Order of the B-Tree
 
-        void divideNoFilhoCheio(BTreeNode<T>* atual, int posicao){
-            BTreeNode<T>* esquerda = atual->filhos[posicao];
-            BTreeNode<T>* direita = new BTreeNode<T>(ordem,esquerda->ehFolha);
-            direita->n = ordem; //ordem * 2 / 2
+    // Function to split a full child node
+    void splitChild(BTreeNode<T>* x, int i) { //i = posição do ponteiro do filho
+        BTreeNode<T>* y = x->children[i];
+        BTreeNode<T>* z = new BTreeNode<T>(order, y->leaf);
+        z->n = order;
 
-            for(int j=0; j<ordem; j++)
-                direita->chaves[j] = esquerda->chaves[j+ordem];
+        for (int j = 0; j < order; j++)
+            z->keys[j] = y->keys[j + order];
 
-            if(!esquerda->ehFolha){
-                for(int i=0; i<ordem; i++)
-                    direita->filhos[i] = esquerda->filhos[i+ordem];
-            }
-
-            esquerda->n = ordem-1;
-
-            for(int i = atual->n - 1; i >= posicao; i--) {
-                if (i + 1 < ordem * 2)
-                    atual->chaves[i + 1] = atual->chaves[i];
-            }
-
-            atual->chaves[posicao] = esquerda->chaves[ordem-1];
-
-            for(int i=atual->n; i>= posicao+1; i--)
-                atual->filhos[i+1] = atual->filhos[i];
-
-            atual->filhos[posicao+1] = direita;
-            atual->filhos[posicao] = esquerda;
-
-            (atual->n)++;
-
+        if (!y->leaf) {
+            for (int j = 0; j <= order; j++)
+                z->children[j] = y->children[j + order];
         }
 
-        void insereNoNaoCheio(BTreeNode<T>* noAtual, T novo){
-            int posicao = noAtual->n-1;
-            if(noAtual->ehFolha){
-                while(posicao >= 0 && novo < noAtual->chaves[posicao]){
-                    noAtual->chaves[posicao+1] = noAtual->chaves[posicao];
-                    posicao--;
-                }
-                noAtual->chaves[posicao+1] = novo;
-                (noAtual->n)++;
+        y->n = order/2;
+        x->children.insert(x->children.begin() + i + 1, z);
 
+        x->keys.insert(x->keys.begin() + i, y->keys[order/2]);
+        x->n = x->n + 1;
+    }
+
+    // Function to insert a key in a non-full node
+    void insertNonFull(BTreeNode<T>* x, T k) {
+        int i = x->n - 1;
+
+        if (x->leaf) {
+            while (i >= 0 && k < x->keys[i]) {
+                x->keys[i + 1] = x->keys[i];
+                i--;
             }
-            else{
-                while(posicao >= 0 && novo < noAtual->chaves[posicao])
-                    posicao--;
-                posicao++;
-                if(noAtual->filhos[posicao]->n == ordem*2){
-                    divideNoFilhoCheio(noAtual, posicao);
-                    if(novo > noAtual->chaves[posicao])
-                        posicao++;
-                }
-                insereNoNaoCheio(noAtual->filhos[posicao], novo);
+
+            x->keys[i + 1] = k;
+            x->n = x->n + 1;
+        } else {
+            while (i >= 0 && k < x->keys[i])
+                i--;
+
+            i++;
+            if (x->children[i]->n == 2 * order) {
+                splitChild(x, i);
+
+                if (k > x->keys[i])
+                    i++;
             }
+            insertNonFull(x->children[i], k);
         }
+    }
 
-        void imprime(BTreeNode<T> raiz){
-            cout << "(";
-            for(int i=0; i<raiz.n; i++){
-                cout << raiz.chaves[i];
-                if(i+1 < raiz.n)
+    void imprime(BTreeNode<T>* root){
+        cout << "(";
+        for(int i=0; i<root->n; i++){
+            cout << root->keys[i];
+            if(i+1 < root->n)
+                cout << ",";
+        }
+        cout << ")";
+        if(!root->leaf){
+            cout << "[ ";
+            for(int i=0; i<=root->n; i++){
+                imprime(root->children[i]);
+                if(i < root->n)
                     cout << ",";
             }
-            cout << ")";
-            if(!raiz.ehFolha){
-                cout << "[";
-                for(int i=0; i<=raiz.n; i++){
-                    imprime(*raiz.filhos[i]);
-                    if(i < raiz.n)
-                        cout << ",";
-                }
-                cout << "]";
-            }
+            cout << " ]";
         }
+    }
 
-    public:
-        BTree(int ordem) : ordem(ordem){
-            raiz = new BTreeNode<T>(ordem, true);
+public:
+    BTree(int order) : order(order) { root = new BTreeNode<T>(order, true); }
+
+    // Function to insert a key in the tree
+    void insert(T k) {
+        int posicao = 0;
+        for(int i=0; i<root->n; i++){
+            if(root->keys[i] < k)
+                posicao++;
         }
+        bool filhoTemEspaco = false;
 
-        void insere(T novo){
-            int posicao = 0;
-            for(int i=0; i<raiz->n; i++){
-                if(raiz->chaves[i] < novo)
-                    posicao++;
-            }
-            bool filhoTemEspaco = false;
+        if (root->children[posicao] != nullptr && root->children[posicao]->n < order * 2)
+            filhoTemEspaco = true;
 
-            if (raiz->filhos[posicao] != nullptr && raiz->filhos[posicao]->n < ordem * 2)
-                filhoTemEspaco = true;
+        if (root->n == 2 * order && !filhoTemEspaco) {
+            BTreeNode<T>* s = new BTreeNode<T>(order, false);
+            s->children[0] = root;
+            root = s;
+            splitChild(root, 0);
+            insertNonFull(s, k);
+        } else
+            insertNonFull(root, k);
+    }
 
-            if(raiz->n == ordem*2 && !filhoTemEspaco){
-                    cout << novo << "-1" << endl;
-                BTreeNode<T>* novoNo = new BTreeNode<T>(ordem, false);
-                novoNo->filhos[0] = raiz;
-                raiz = novoNo;
-                divideNoFilhoCheio(novoNo,0);
-                insereNoNaoCheio(novoNo, novo);
-            }
-            else{
-                cout << novo << "-2" << endl;
-                insereNoNaoCheio(raiz, novo);
-            }
-        }
-
-        void imprimeArvore(){
-            imprime(*raiz);
-            cout << endl;
-        }
+    void imprimeArvore(){
+        imprime(root);
+        cout << endl;
+    }
 
 };
 
 int main() {
-    vector<int> numbers = {20,40,10,30,15,35,7,26,18,22,5,42,13,46,27,8,32,38,24,45,25};
-    BTree<int> arvore(2);
+    BTree<int> t(2);
 
-    arvore.insere(20);
-    arvore.insere(40);
-    arvore.insere(10);
-    arvore.insere(30);
-    arvore.insere(15);
-    arvore.insere(35);
-    arvore.insere(7);
-    arvore.insere(26);
-    arvore.insere(18);
-    arvore.imprimeArvore();
-    arvore.insere(22);
-    
-    arvore.insere(5);
-    arvore.insere(42);
-    arvore.insere(13);
-    arvore.insere(46);
-    arvore.insere(27);
-    arvore.insere(8);
-    arvore.insere(32);
-    arvore.imprimeArvore();
-    arvore.insere(38);
-    arvore.insere(24);
-    arvore.insere(45);
-    arvore.insere(25);
-
-
-    arvore.imprimeArvore();
+    t.insert(20);
+    t.insert(40);
+    t.insert(10);
+    t.insert(30);
+    t.insert(15);
+    t.insert(35);
+    t.insert(7);
+    t.insert(26);
+    t.insert(18);
+    t.insert(22);
+    t.insert(5);
+    t.insert(42);
+    t.insert(13);
+    t.insert(46);
+    t.insert(27);
+    t.insert(8);
+    t.insert(32);
+    t.insert(38);
+    t.insert(24);
+    t.insert(45);
+    t.insert(25);
+    t.imprimeArvore();
 
     return 0;
 }
